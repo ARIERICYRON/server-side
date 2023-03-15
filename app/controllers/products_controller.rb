16011f02e61796625af09cfc5f74abbf83b9e7a3
ariericyron <ariericyron@gmail.com>
2023-03-15 07:42:12 +0300

    feat:created the get request

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,15 @@
 class ProductsController < ApplicationController
+    def index
+        products = Product.all
+        render json: products
+    end
+
+    def show
+        product = Product.find_by(id: params[:id])
+        if product
+            render json: product
+        else
+            render json: { error: "Product not found" }, status: :not_found
+        end
+    end
 end
