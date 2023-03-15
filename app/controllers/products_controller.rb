1559d06dfbfd059f879e238b9b31f01b1137ed37
ariericyron <ariericyron@gmail.com>
2023-03-15 07:44:38 +0300

    feat:created delete and post request

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,4 +12,25 @@ class ProductsController < ApplicationController
             render json: { error: "Product not found" }, status: :not_found
         end
     end
+
+    def create
+        product = Product.create!(product_params)
+        render json: product
+    end
+
+    def destroy
+        product = Product.find_by(id: params[:id])
+        if product
+            product.destroy
+            head :no_content
+        else
+            render json: {error: 'Product not found' }, status: :not_found
+        end
+    end
+
+    private
+
+    def product_params
+        params.permit(:image_url, :sku, :name, :price)
+    end
 end
